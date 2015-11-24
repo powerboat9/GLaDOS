9911ca0a058968d843c6719fe6f284a7590893ad
powerboat9 <powerboat9@users.noreply.github.com>
2015-11-24 14:31:24 -0800

    Update elevator.lua

--- input/elevator.lua
+++ input/elevator.lua
@@ -1,2 +1,25 @@
-function onRednet(msg, id, protocol)
-    --Do later
+#import output elevatorControl
+#import output floorIndicator
+local requests = {}
+
+function onElevatorCallEvent(fromFloor, direction)
+    --Finnish Later
+
+function onCrash(id)
+    local computer
+    
+    for v in {peripheral.find("computer")} do
+        if v.getID() == id then
+            computer = v
+            break
+        end
+    end
+    if not computer then
+        isDisconected = true
+        return false, "connection_fail"
+    elseif not computer.isOn()
+        computer.turnOn()
+    else
+        computer.reboot()
+    end
+end
